d8e7b6eda727a6d637981314cdece040c2cb2d94
BrianDelamarter <brian.d.delamarter@gmail.com>
2025-01-13 16:32:16 -0800

    Changes made a while ago to try and fix bugs

--- Final Project/final.py
+++ Final Project/final.py
@@ -2,6 +2,7 @@ import zipfile
 
 
 from PIL import Image
+from PIL import ImageDraw
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR/tesseract.exe"
 import cv2 as cv
@@ -24,6 +25,25 @@ def createDictionary(fileLocation):
         for info in smallData.infolist():
             pictures[info.filename] = Image.open(smallData.open(info.filename))
 
+def binarize(image_to_transform, threshold = 240):
+    # now, lets convert that image to a single greyscale image using convert()
+    output_image=image_to_transform.convert("L")
+    # the threshold value is usually provided as a number between 0 and 255.
+    # the algorithm for the binarization is pretty simple, go through every pixel in the
+    # image and, if it's greater than the threshold, turn it all the way up (255), and
+    # if it's lower than the threshold, turn it all the way down (0).
+    for x in range(output_image.width):
+        for y in range(output_image.height):
+            # for the given pixel at w,h, lets check its value against the threshold
+            if output_image.getpixel((x,y))< threshold: #note that the first parameter is actually a tuple object
+                # lets set this to zero
+                output_image.putpixel( (x,y), 0 )
+            else:
+                # otherwise lets set this to 255
+                output_image.putpixel( (x,y), 255 )
+    #now we just return the new image
+    return output_image
+
 def searchPicture(picture, critera):
     '''Takes an image and a search term as arguements. It will then convert the picture to the most advantageous form to you 
     Pytesseract to extract all text from the picture. Then it will look to see if the search terms is on that page. If so it will
@@ -32,24 +52,31 @@ def searchPicture(picture, critera):
     ::param picture is a PIL object picture
     ::param search term is a word to search for in the picture
     '''
-
-    text = pytesseract.image_to_string(picture)
+    binarized = binarize(picture)
+    text = pytesseract.image_to_string(binarized)
     for word in text.split():
         if word.upper() == critera.upper():
             return True
     return False
     
 def getFaces(imgSearch):
-    '''Takes a list of all pictures that contain the search criteria and will extract all faces from them
+    '''Takes a list of all pictures keys (file names) that contain the search criteria and will extract all faces from them
     
     ::param hits is a list of pictures that contain search crtieria'''
 
     face_cascade = cv.CascadeClassifier("Final Project\haarcascade_frontalface_default.xml")
     for pic in imgSearch:
-        x = cv.cvtColor(np.array(pictures[pic]), cv.COLOR_RGB2BGR)
-        gray = cv.cvtColor(x, cv.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray)
-        faces
+        grey = pictures[pic].convert('L')
+        x = np.array(grey)
+        faces = face_cascade.detectMultiScale(x)
+        drawing=ImageDraw.Draw(pictures[pic])
+        for a in faces:
+            aList = a.tolist()
+            x,y,w,h = aList      
+            drawing.rectangle((x,y,x+w,y+h), outline="white")
+        pictures[pic].show()
+            
+                
 
 
 
@@ -58,7 +85,8 @@ if __name__ == '__main__':
     createDictionary(location)
     search = "application" 
     hits = []
-    for img in pictures.keys():
+    '''for img in pictures.keys():
         if searchPicture(pictures[img], search):
-            hits.append(img)
-    getFaces(hits)
+            hits.append(img)'''
+    pic = ['a-0.png']
+    getFaces(pic)
